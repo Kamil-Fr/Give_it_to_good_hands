88283aee035d66bec79f051936f7f35e14066849
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2023-09-14 23:47:11 +0200

    Change file WhoWeHelp.

--- src/components/WhoWeHelp.jsx
+++ src/components/WhoWeHelp.jsx
@@ -1,161 +1,181 @@
-import {useState} from "react";
+import { useState } from "react";
 import "../scss/_whoWeHelp.scss";
 import decoration from "../assets/Decoration.svg";
 
+const organizationsData = {
+    "Fundacje": [
+        {
+            name: "Fundacja “Dbam o Zdrowie”",
+            mission: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+            needs: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+        },
+        {
+            name: "Fundacja “Dla dzieci”",
+            mission: "Pomoc dzieciom z ubogich rodzin.",
+            needs: "ubrania, meble, zabawki",
+        },
+        {
+            name: "Fundacja “Bez domu”",
+            mission: "Pomoc dla osób nie posiadających miejsca zamieszkania.",
+            needs: "ubrania, jedzenie, ciepłe koce",
+        },
+        {
+            name: "Fundacja “Dbam o Zdrowie”",
+            mission: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+            needs: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+        },
+        {
+            name: "Fundacja “Dla dzieci bez domu”",
+            mission: "Pomoc dzieciom z ubogich rodzin.",
+            needs: "ubrania, meble, zabawki",
+        },
+        {
+            name: "Fundacja “Bez domu”",
+            mission: "Pomoc dla osób nie posiadających miejsca zamieszkania.",
+            needs: "ubrania, jedzenie, ciepłe koce",
+        },
+        {
+            name: "Fundacja “Dbam o Zdrowie”",
+            mission: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+            needs: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+        },
+        {
+            name: "Fundacja “Dla dzieci”",
+            mission: "Pomoc dzieciom z ubogich rodzin.",
+            needs: "ubrania, meble, zabawki",
+        },
+        {
+            name: "Fundacja “Bez domu”",
+            mission: "Pomoc dla osób nie posiadających miejsca zamieszkania.",
+            needs: "ubrania, jedzenie, ciepłe koce",
+        },
+    ],
+    "Organizacje pozarządowe": [
+        {
+            name: "Organizacja “Lorem Ipsum 1”",
+            mission: "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+            needs: "Egestas, sed, tempus",
+        },
+        {
+            name: "Organizacja “Lorem Ipsum 2”",
+            mission: "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+            needs: "Ut, aliquam, purus, sit, amet",
+        },
+        {
+            name: "Organizacja “Lorem Ipsum 3”",
+            mission: "Scelerisque in dictum non consectetur a erat nam.",
+            needs: "Mi, quis, hendrerit, dolor",
+        },
+        {
+            name: "Organizacja “Lorem Ipsum 1”",
+            mission: "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+            needs: "Egestas, sed, tempus",
+        },
+        {
+            name: "Organizacja “Lorem Ipsum 2”",
+            mission: "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+            needs: "Ut, aliquam, purus, sit, amet",
+        },
+        {
+            name: "Organizacja “Lorem Ipsum 3”",
+            mission: "Scelerisque in dictum non consectetur a erat nam.",
+            needs: "Mi, quis, hendrerit, dolor",
+        },
+    ],
+    "Lokalne zbiórki": [
+        {
+            name: "Fundacja “Lorem Ipsum 1”",
+            mission: "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+            needs: "Egestas, sed, tempus, gestas, sed, tempus",
+        },
+        {
+            name: "Fundacja “Lorem Ipsum 2”",
+            mission: "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+            needs: "Ut, aliquam, purus, sit, amet, Ut, aliquam, purus, sit, amet",
+        },
+        {
+            name: "Fundacja “Lorem Ipsum 3”",
+            mission: "Scelerisque in dictum non consectetur a erat nam.",
+            needs: "Mi, quis, hendrerit, dolor, Mi, quis, hendrerit, dolor",
+        },
+    ],
+};
+
+const itemsPerPage = 3;
+
 function WhoWeHelp() {
+    const [activeCategory, setActiveCategory] = useState("Fundacje");
+    const [currentPage, setCurrentPage] = useState(1);
 
-    const [dispFirst, setDispFirst] = useState('block')
-    const [bordFirst, setBordFirst] = useState('1px solid black')
+    const handleCategoryClick = (category) => {
+        setActiveCategory(category);
+        setCurrentPage(1);
+    };
 
-    const [dispSecond, setDispSecond] = useState('none')
-    const [bordSecond, setBordSecond] = useState('none')
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = organizationsData[activeCategory].slice(
+        indexOfFirstItem,
+        indexOfLastItem
+    );
 
-    const [dispThird, setDispThrird] = useState('none')
-    const [bordThird, setBordThird] = useState('none')
+    const totalPages = Math.ceil(
+        organizationsData[activeCategory].length / itemsPerPage
+    );
 
-
-    const dispNoneStyleFirst = {
-        display: dispFirst,
-    }
-    const borderFirst = {
-        border: bordFirst
-    }
-
-    const dispNoneStyleSecond = {
-        display: dispSecond
-    }
-    const borderSecond = {
-        border: bordSecond
-    }
-
-    const dispNoneStyleThird = {
-        display: dispThird
-    }
-    const borderThird = {
-        border: bordThird
-    }
-
-    const handleClickFirstOption = () => {
-        setDispFirst('block')
-        setDispSecond('none')
-        setDispThrird('none')
-        setBordFirst('1px solid black')
-        setBordSecond('none')
-        setBordThird('none')
-    }
-
-    const handleClickSecondOption = () => {
-        setDispSecond('block')
-        setDispFirst('none')
-        setDispThrird('none')
-        setBordSecond('1px solid black')
-        setBordFirst('none')
-        setBordThird('none')
-    }
-
-    const handleClickThirdOption = () => {
-        setDispThrird('block')
-        setDispFirst("none")
-        setDispSecond('none')
-        setBordThird('1px solid black')
-        setBordFirst('none')
-        setBordSecond('none')
-    }
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     return (
         <section id="whoWeHelp" className="whoWeHelp">
             <div className="headerWhoHelp">
                 <h1>Komu pomagamy?</h1>
-                <img src={decoration} alt="decoration"/>
+                <img src={decoration} alt="decoration" />
             </div>
             <div className="switchWhoHelp">
-                <button onClick={handleClickFirstOption} style={borderFirst}>Fundacjom</button>
-                <button onClick={handleClickSecondOption} style={borderSecond}>Organizacjom<br/>pozarządowym</button>
-                <button onClick={handleClickThirdOption} style={borderThird}>Lokalny<br/>zbiórkom</button>
-            </div>
-            <p className="articleP">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+                {Object.keys(organizationsData).map((category) => (
+                    <button
+                        key={category}
+                        onClick={() => handleCategoryClick(category)}
+                        className={activeCategory === category ? "active" : ""}
+                    >
+                        {category}
+                    </button>
+                ))}
+
+                                </div>
+            <p className="articleP">
+                W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi
+                współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i
+                czego potrzebują.
+            </p>
             <div className="viewInfo">
-                <div className="firstDivInfo" style={dispNoneStyleFirst}>
-                    <div>
+                {currentItems.map((organization, index) => (
+                    <div key={index} className={index === 2 ? "noBorder" : ""}>
                         <div>
-                            <h1>Fundacja “Dbam o Zdrowie”</h1>
-                            <p>Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.</p>
+                            <h1>{organization.name}</h1>
+                            <p>Cel i misja: {organization.mission}</p>
                         </div>
-                        <p>ubrania, jedzenie, sprzęt AGD, meble, zabawki</p>
+                        <p>{organization.needs}</p>
                     </div>
-                    <div>
-                        <div>
-                            <h1>Fundacja “Dla dzieci”</h1>
-                            <p>Cel i misja: Pomoc dzieciom z ubogich rodzin.</p>
-                        </div>
-                        <p>ubrania, meble, zabawki</p>
-                    </div>
-                    <div id="withoutBorder">
-                        <div>
-                            <h1>Fundacja “Bez domu”</h1>
-                            <p>Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.</p>
-                        </div>
-                        <p>ubrania, jedzenie, ciepłe koce</p>
-                    </div>
-                                     <div className="pageNumber" id="withoutBorder">
-                        <span onClick={handleClickFirstOption}>1</span>
-                        <span onClick={handleClickSecondOption}>2</span>
-                        <span onClick={handleClickThirdOption}>3</span>
-                    </div>
-                </div>
-                <div className="secondDivInfo" style={dispNoneStyleSecond}>
-                    <div>
-                        <div>
-                            <h1>Organizacja “Lorem Ipsum 1”</h1>
-                            <p>Quis varius quam quisque id diam vel quam elementum pulvinar.</p>
-                        </div>
-                        <p>Egestas, sed, tempus</p>
-                    </div>
-                    <div>
-                        <div>
-                            <h1>Organizacja “Lorem Ipsum 2”</h1>
-                            <p>Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</p>
-                        </div>
-                        <p>Ut, aliquam, purus, sit, amet</p>
-                    </div>
-                    <div id="withoutBorder">
-                        <div>
-                            <h1>Organizacja “Lorem Ipsum 3”</h1>
-                            <p>Scelerisque in dictum non consectetur a erat nam.</p>
-                        </div>
-                        <p>Mi, quis, hendrerit, dolor</p>
-                    </div>
-                    <div className="pageNumber">
-                        <span onClick={handleClickFirstOption}>1</span>
-                        <span onClick={handleClickSecondOption}>2</span>
-                    </div>
-                </div>
-                <div className="thirdDivInfo" style={dispNoneStyleThird}>
-                    <div>
-                        <div>
-                            <h1>Fundacja “Lorem Ipsum 1”</h1>
-                            <p>Quis varius quam quisque id diam vel quam elementum pulvinar.</p>
-                        </div>
-                        <p>Egestas, sed, tempus, gestas, sed, tempus</p>
-                    </div>
-                    <div>
-                        <div>
-                            <h1>Fundacja “Lorem Ipsum 2”</h1>
-                            <p>Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</p>
-                        </div>
-                        <p>Ut, aliquam, purus, sit, amet, Ut, aliquam, purus, sit, amet</p>
-                    </div>
-                    <div id="withoutBorder">
-                        <div>
-                            <h1>Fundacja “Lorem Ipsum 3”</h1>
-                            <p>Scelerisque in dictum non consectetur a erat nam.</p>
-                        </div>
-                        <p>Mi, quis, hendrerit, dolor, Mi, quis, hendrerit, dolor</p>
-                    </div>
-                </div>
+                ))}
             </div>
+            {totalPages > 1 && (
+                <div className="pageNumber">
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <span
+                            key={index}
+                            onClick={() => handlePageClick(index + 1)}
+                            className={currentPage === index + 1 ? "activePage" : ""}
+                        >
+              {index + 1}
+            </span>
+                    ))}
+                </div>
+            )}
         </section>
-    )
+    );
 }
 
 export default WhoWeHelp;
