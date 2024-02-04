dd4584cad65890e1ee6905eead87d51b4a657929
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-02-04 15:19:14 +0100

    Add second translation.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,9 +7,11 @@ function AboutUs() {
     return (
         <section id="aboutUs" className="aboutUs">
             <div className="leftSideAboutUs">
-                <h1>O nas</h1>
+                <h1>About us</h1>
                 <img src={decoration} alt="decoration"/>
-                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <p>The creators of the website dedicated to facilitating the donation of unwanted items are passionate individuals committed to fostering a culture of reuse and reducing waste in our communities.</p>
+                <p>Motivated by a shared vision of creating a platform that connects those with surplus belongings to those in need, the website's founders possess a deep sense of social responsibility and environmental consciousness.</p>
+                <p>Through their innovative approach and dedication to making a positive impact, the team behind the website for unwanted item donation strives to build a more sustainable and compassionate world, one where resources are shared to benefit those who need them the most.</p>
                 <img className="signatureImage" src={signature} alt="signature"/>
             </div>
             <div className="rightSideAboutUs">
